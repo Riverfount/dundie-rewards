7e019f8648b765642331b431a9fee882332c0fc8
Vicente Marçal <vicente.marcal@gmail.com>
2022-07-20 11:30:27 -0300

    issue-8 Set up project initial organization and metadata
    - Add setup.py to turn installable
    - Add __init__.py for package
    - Add __main__.py for executable
    
    closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dundie',
+    version='0.1.0',
+    description='Reward Point System for Dunder Mifflin',
+    author='Vicente Marçal',
+    packages=find_packages(),
+)
