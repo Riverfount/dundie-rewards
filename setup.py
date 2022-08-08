9ea4e0c436500fc6f628fd2f18c944f51a4dbf8b
Vicente Marçal <vicente.marcal@gmail.com>
2022-08-08 09:22:26 -0300

    feat(tests): Added dependencies management and Makefile
    It was configured Github Actions.
    
    Closes #10

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
     author='Vicente Marçal',
+    python_requires='>=3.8',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
