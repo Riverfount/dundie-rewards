cfd555f90931540d75fe5b2b93a8f27af025191f
Vicente Marçal <vicente.marcal@gmail.com>
2022-08-20 11:45:14 -0300

    refactor(CLI): Refactor to use Click and Rich
    We refactored the code to implement our load command using the python
    libraries Click and Rich.
    
    Closes #18

--- dundie/cli.py
+++ dundie/cli.py
@@ -1,26 +1,44 @@
-import argparse
+import pkg_resources
+import rich_click as click
+from rich.console import Console
+from rich.table import Table
 
-from dundie.core import load  # noqa
+from dundie import core
+
+click.rich_click.APPEND_METAVARS_HELP = True
+click.rich_click.GROUP_ARGUMENTS_OPTIONS = True
+click.rich_click.SHOW_ARGUMENTS = True
+click.rich_click.SHOW_METAVARS_COLUMN = True
+click.rich_click.USE_MARKDOWN = True
+click.rich_click.USE_RICH_MARKUP = True
 
 
+@click.group()
+@click.version_option(pkg_resources.get_distribution('dundie').version)
 def main():
-    parser = argparse.ArgumentParser(
-        description='Dunder Mifflin Rewards CLI',
-        epilog='Enjoy and use wiht cautious.',
-    )
-    parser.add_argument(
-        'subcommand',
-        type=str,
-        help='The subcommand to run',
-        choices=('load', 'show', 'send'),
-        default='help'
-    )
-    parser.add_argument(
-        'filepath',
-        type=str,
-        help='File path to load.',
-        default=None
-    )
+    """Dunder Mifflin Rewards System
 
-    args = parser.parse_args()
-    print(*globals()[args.subcommand](args.filepath))
+    This cli application controls DM rewards.
+    """
+
+
+@main.command()
+@click.argument('filepath', type=click.Path(exists=True))
+def load(filepath):
+    """Loads the file to database.
+    ## Features
+    - Validantes data
+    - Pareses the file
+    - Load database
+    """
+
+    table = Table(title='Dunder Mifflin Associantes')
+    headers = ['name', 'dept.', 'role', 'e-mail']
+    for header in headers:
+        table.add_column(header, style="magenta")
+
+    result = core.load(filepath)
+    for person in result:
+        table.add_row(*[field.strip() for field in person.split(',')])
+    console = Console()
+    console.print(table)
