910d3f037e4adc2cf7030b641b7c603384b7539e
Vicente Marçal <vicente.marcal@gmail.com>
2022-08-21 19:53:27 -0300

    chore(CLI): Improvements to build and publish the project at PYPI
    Adjustments in the code as following:
    - rename the project at setup.py,
    - put the new name in click version decorator,
    - installed the libs wheel and twice to build and publish the project at pypi
    - created the new commands build and publish-test in Makefile
    - created the file MANIFEST.in to include assets/people.csv in project build
    
    Closes #26

--- setup.py
+++ setup.py
@@ -26,14 +26,15 @@ def read_requirements(path):
 
 
 setup(
-    name='dundie',
+    name='vm-dundie',
     version='0.1.0',
     description='Reward Point System for Dunder Mifflin',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
     author='Vicente Marçal',
     python_requires='>=3.8',
-    packages=find_packages(),
+    packages=find_packages(exclude=['integration']),
+    include_package_data=True,
     entry_points={
         'console_scripts': [
             'dundie = dundie.__main__:main'
