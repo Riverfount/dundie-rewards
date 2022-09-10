471eea361dd2947ff19155044de412f944f854f2
Vicente Marçal <vicente.marcal@gmail.com>
2022-09-10 15:17:10 -0300

    chore(database): Configurada aplicação para gerencia das migrações de banco de dados.
    Instalada e configurada a lib alembic para gerenciar as migrações de
    banco de dados.
    
    Closes #32

--- dundie/settings.py
+++ dundie/settings.py
@@ -7,6 +7,6 @@ SMTP_TIMEOUT = 5
 EMAIL_FROM = 'master@dundie.com'
 
 ROOT_PATH = os.path.dirname(__file__)
-DATABASE_PATH = os.path.join(ROOT_PATH, '..', 'assets', 'database.DB')
+DATABASE_PATH = os.path.join(ROOT_PATH, '..', 'assets', 'database.db')
 SQL_CONN_STRING: str = f'sqlite:///{DATABASE_PATH}'
 DATEFMT: str = '%d/%m/%Y %H:%M:%S'
